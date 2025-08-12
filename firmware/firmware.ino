8f1c112f9d4af0bb0e0340479b70dfa985a5c44b
Jeff Merola <70715253+senrath@users.noreply.github.com>
2025-08-11 21:50:43 -0400

    Initial upload of arduino ide version of the firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -8,8 +8,6 @@
  * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
  */
 
-#include <Arduino.h>
-
 #include "NewEncoder.h"
 
 #include <SPI.h>
